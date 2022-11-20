8688a74a77768853de2a80adf4913a405df58df9
m-triple-m <triplem656@mail.com>
2022-11-20 13:43:13 +0530

    ..

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const app_config = {
-    api_url : 'http://localhost:5000',
-    // api_url : 'https://generator-api.onrender.com',
+    // api_url : 'http://localhost:5000',
+    api_url : 'https://generator-api.onrender.com',
     courses : [
         'Python Data Science',
         'MERN Stack Development',
